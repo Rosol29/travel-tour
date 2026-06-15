6612734a967f6af7b082cc89d1f69e0cf0a960a5
Rosol29 <rosolmehdawi22@hotmail.com>
2026-06-15 07:42:32 -0400

    add all

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -10,7 +10,13 @@ function searchCondition() {
       .then(response => response.json())
       .then(data => {
         const country = data.countries;
+        const beach = data.beaches;
+        const temple = data.temples;
+
+
         console.log('country',country);
+        console.log('beach',beach);
+
         if(input === "country"){
            
             for(let i=0; i<= country.length-1; i++){
@@ -20,28 +26,60 @@ function searchCondition() {
                const imageu=subcity[j].imageUrl;
                const descriptioncity=subcity[j].description || subcity[j][""];
                resultDiv.innerHTML +=`<div class="card">
-               <img src="${imageu}" style="width:100%; height:100%" alt="hjh">
+               <img src="${imageu}" style="width:100%; height:200px" alt="hjh">
                <h3><b>${city}</b></h3>
                <p>${descriptioncity}</p>
                </div>`;
-            //    resultDiv.innerHTML += `<img src="${imageu}" alt="hjh">`;
-
-            //    resultDiv.innerHTML +=`<h1>${city}</h1>`;
-               
 
                console.log('city name',city)
                 }
             
-
-                // console.log('name',subcity)
-          
-                // console.log('city2 name',city2)
-
                 
             }
            
         }
+
+        if(input.toLowerCase() === "beach"){
+            console.log('BEACH',beach)
+            for(let i=0; i<= beach.length-1; i++){
+                const subcity=beach[i].name;
+                const imageu=beach[i].imageUrl;
+                const descriptioncity=beach[i].description;
+      
+             
+               resultDiv.innerHTML +=`<div class="card">
+               <img src="${imageu}" style="width:100%; height:100%" alt="hjh">
+               <h3><b>${subcity}</b></h3>
+               <p>${descriptioncity}</p>
+               </div>`;
+
+            
+                
+            
+                
+            }
+        }
     
+        if(input.toLowerCase() === "temple"){
+            console.log('temple',temple)
+            for(let i=0; i<= temple.length-1; i++){
+                const subcity=temple[i].name;
+                const imageu=temple[i].imageUrl;
+                const descriptioncity=temple[i].description;
+      
+             
+               resultDiv.innerHTML +=`<div class="card">
+               <img src="${imageu}" style="width:100%; height:200px" alt="hjh">
+               <h3><b>${subcity}</b></h3>
+               <p>${descriptioncity}</p>
+               </div>`;
+
+            
+                
+            
+                
+            }
+        }
 
       })
       .catch(error => {
